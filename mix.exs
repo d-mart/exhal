8b66a8c3b9ffae5748ca5393ce9bae895a0b5c7a
David Martinez <dave.h.martinez@gmail.com>
2018-07-18 10:52:31 -0400

    Update exvcr and httpoison deps

--- mix.exs
+++ mix.exs
@@ -22,13 +22,13 @@ defmodule ExHal.Mixfile do
     [
       {:poison, "~> 2.2 or ~> 3.0"},
       {:uri_template, "~> 1.0"},
-      {:httpoison, "~> 0.11 or ~> 1.0"},
+      {:httpoison, "~> 0.13 or ~> 1.2"},
       {:odgn_json_pointer, "~> 1.0.0", app: false},
       {:expat, "~> 1.0"},
       {:earmark, ">= 0.0.0", only: :dev},
       {:ex_doc, "~> 0.11", only: :dev},
       {:dialyxir, "~> 0.3", only: :dev, runtime: false},
-      {:exvcr, "~> 0.7", only: :test},
+      {:exvcr, "~> 0.10", only: :test},
       {:excoveralls, "~> 0.4", only: :test},
       {:mox, "~> 0.3", only: :test},
       {:stream_data, "~> 0.1", only: :test}
